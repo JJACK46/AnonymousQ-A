f2a2a8f1f906942383b4b3a8adec8c73c199a6bd
Aphidech Mahakoat <jjack46.flowsick@gmail.com>
2024-08-07 09:07:25 +0700

    update structure

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -37,9 +37,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "api",
-    "rest_framework",
-    "corsheaders"
 ]
 
 MIDDLEWARE = [
@@ -50,7 +47,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "myproject.urls"
